360c0e1fb1a26481a0c804c26d03e65abe90d4ef
Lorena C <lorena@eunknown.org>
2019-04-20 18:24:09 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each(words)
+  counter = 0
+  while counter < words.length
+  yield(words[counter])
+  counter +=1
+  end
+  return words
 end
